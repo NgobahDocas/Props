15e5a87d8d3b13fa90f85c896a2b447fd23f6ec4
unknown <ngobahdocas@gmail.com>
2024-07-30 00:55:03 -0700

    Add props work to GitHub

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import Greet from './Components/Greet';
+import Welcome from './Components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Greet name="Bruce" heroName="Batman"> </Greet>
+      <Greet name="Clark" heroName="Superman "></Greet>
+      <Greet name="Diana" heroName="Wonder Woman"></Greet>
+      <Welcome  name="Bruce" heroName="Batman" />
+      <Welcome name="Clark" heroName="Superman "/>
+      <Welcome  name="Diana" heroName="Wonder Woman"/>
     </div>
   );
 }
